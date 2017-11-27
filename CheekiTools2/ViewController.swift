99d2d310b48a4b12607f892869c1d9ca8d59b52b
BuildTools <unconfigured@null.spigotmc.org>
2017-11-27 15:49:01 -0500

    Added Sandbox Checking, OTA Update Blocking, and semi-functional NonceSetter.

--- CheekiTools2/ViewController.swift
+++ CheekiTools2/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,4 +22,42 @@ class ViewController: UIViewController {
 
 
 }
+class TvOsBeta: UIViewController {
+    @IBAction func install(sender: AnyObject) {
+        var url: NSURL
+        url=NSURL(string: "https://cdn.discordapp.com/attachments/356133677085753347/359766221274480642/tvOS_11_beta_Profile.mobileconfig")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+}
 
+class APNonce: UIViewController {
+    @IBOutlet weak var rootpwd: UITextField!
+    
+    @IBOutlet weak var noncetext: UITextField!
+    var variableString = "nvram com.apple.System.boot-nonce="
+    
+    @IBAction func setnonce(sender: AnyObject) {
+        variableString += noncetext.text
+        system(variableString)
+    }
+}
+
+class JBChecker: UIViewController {
+    var isDir2 : ObjCBool = true
+    var fileManager = NSFileManager()
+    var tmpDir = NSTemporaryDirectory()
+   
+    var isDir : ObjCBool = true
+    @IBOutlet weak var jbstatus: UILabel!
+    override func viewDidLoad() {
+                    if fileManager.fileExistsAtPath("/Applications/SpringBoard.app/", isDirectory:&isDir) {
+                NSLog("filemansuccess")
+                jbstatus.text = "Root Files can be read from a sandboxed app"
+                
+            } else {
+                jbstatus.text = "Root Files can NOT be read from a sandboxed app"
+            }
+        
+    }
+    }
